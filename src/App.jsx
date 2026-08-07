36f4bad9e95e83cc60ac4713921c45fb07dce615
Aishwarya Tandon <aishutandon.at@gmail.com>
2026-08-06 21:41:24 -0400

    Update website

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-=======
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import { FaLinkedin, FaEnvelope } from "react-icons/fa";
@@ -383,4 +346,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 6c2677de8fd93d35b11de99a77940e85a3f01549
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
 import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
 root.render(
   <React.StrictMode>
@@ -25,5 +13,4 @@ root.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
->>>>>>> 6c2677de8fd93d35b11de99a77940e85a3f01549
+)
